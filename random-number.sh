6d53a6fd9041adba3d2e5285244ead512c7ab986
Eduardo <53496439+eduardbuzzi@users.noreply.github.com>
2020-03-06 03:29:25 -0300

    Update random-number.sh

--- random-number.sh
+++ random-number.sh
@@ -1,6 +1,7 @@
 read -p "Informe o número mínimo: " numMIN
 read -p "Informe o número máximo: " numMAX
 read -p "Informe o nome do arquivo que será salvo com o log: " logRANDOMnumber
+
 rm -rf $logRANDOMnumber
 mkdir $logRANDOMnumber
 
@@ -11,13 +12,16 @@ wget -q dontpad.com/$NUM.txt
 TRUEorFALSE=$(cat $NUM.txt)
 
 if [ -z "$TRUEorFALSE" ]
+
 then
 echo "Não tem nada dentro do dontpad.com/$NUM"
 rm $NUM.txt
+
 else
 echo "Encontrei algo interessante dentro do dontpad.com/$NUM"
 touch $PWD/$logRANDOMnumber/$NUM
 echo $TRUEorFALSE > $PWD/$logRANDOMnumber/$NUM
 rm $NUM.txt
+
 fi
 done
